737ce600af9a644f315b25c155cb6b9a8aa5a8d5
gdoza5 <gdoza5@gmail.com>
2019-11-05 21:06:15 -0600

    best i could

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@
 
 $('button').on('click', function(){
     let txt = $('input').val();
-    let skill = $(`<li><button class="delete">x</button>${txt}</li>`);
+    let skill = $(`<li><button Id="delete">x</button>${txt}</li>`);
     $('ul').append(skill);
 
+});
+
+$('ul').on('click', '#delete', function(){
+    $(this).closest('li').remove();
 });
